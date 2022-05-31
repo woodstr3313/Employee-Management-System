2764b5e2f2ad35db040919dd7b5b702a0391d112
Travis Woods <woodstr3313@gmail.com>
2022-05-31 16:42:08 -0400

    changed questions for database due to time constraints. Removed old functions and started over writing new functions for new pro mpts.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
-const util = require("util");
+
 const connection = mysql.createConnection(
   {
     host: "localhost",
@@ -13,77 +13,62 @@ const connection = mysql.createConnection(
   console.log("Connected to Employee Database.")
 );
 
-connection.connect((err) => {
+connection.connect(function (err) {
   if (err) throw err;
-  console.log(" ");
-  start();
+  console.log("Connected" + connection.threadId);
+  startPrompt();
 });
 
-const queryAsync = util.promisify(connection.query).bind(connection);
-
 // Prompts user with a list of questions.
-async function start() {
-  const answer = await inquirer.prompt({
-    name: "selectOption",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "View all departments",
-      "View all roles",
-      "View all employees",
-      "Add a department",
-      "Add a role",
-      "Add an employee",
-      "Delete a department",
-      "Delete a role",
-      "Delete an employee",
-      "Update a role salary",
-      "Update employee role",
-      "Update employee manager",
-      "Exit",
-    ],
-  });
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees?",
+          "View all Employees By Deparments",
+          "View all Employees By Roles",
+          "Add Employee?",
+          "Update Employee",
+          "Add Department?",
+          "Add Role?",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees?":
+          viewAllEmployees();
+          break;
 
-  switch (answer.selectOption) {
-    case "View all departments":
-      viewDepartments();
-      break;
-    case "View all roles":
-      viewRoles();
-      break;
-    case "View all employees":
-      viewEmployees();
-      break;
-    case "Add a department":
-      addDepartment();
-      break;
-    case "Add a role":
-      addRole();
-      break;
-    case "Add an employee":
-      addEmployee();
-      break;
-    case "Delete a department":
-      deleteDepartment();
-      break;
-    case "Delete a role":
-      deleteRole();
-      break;
-    case "Delete an employee":
-      deleteEmployee();
-      break;
-    case "Update a role salary":
-      updateSalary();
-      break;
-    case "Update employee role":
-      updateRole();
-      break;
-    case "Update employee manager":
-      updateManager();
-      break;
-    case "Exit":
-      console.log("Goodbye!");
-      Connection.end();
-      break;
-  }
+        case "View All Employees By Departments?":
+          viewAllDepartments();
+          break;
+        case "View all Employees By Roles":
+          viewAllRoles();
+          break;
+
+        case "Add Employee?":
+          addEmployee();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Add Department?":
+          addDepartment();
+          break;
+
+        case "Add Role?":
+          addRole();
+          break;
+      }
+    });
 }
+
+// SHOW ALL EMPLOYEES
+
